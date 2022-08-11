d28399fca4a9c274126f2fcbca86a16594b63813
NBH <hoangphuong191058@gmail.com>
2022-08-11 16:01:46 +0700

    Add files via upload

---
+++ src/control/common.c
@@ -0,0 +1,46 @@
+/***********************************************************************************************************************************
+Common Handler for Control Commands
+***********************************************************************************************************************************/
+#include "build.auto.h"
+
+#include "command/control/common.h"
+#include "common/debug.h"
+#include "config/config.h"
+#include "storage/helper.h"
+
+/**********************************************************************************************************************************/
+String *
+lockStopFileName(const String *stanza)
+{
+    FUNCTION_TEST_BEGIN();
+        FUNCTION_TEST_PARAM(STRING, stanza);
+    FUNCTION_TEST_END();
+
+    String *result = strNewFmt("%s/%s" STOP_FILE_EXT, strZ(cfgOptionStr(cfgOptLockPath)), stanza != NULL ? strZ(stanza) : "all");
+
+    FUNCTION_TEST_RETURN(result);
+}
+
+/**********************************************************************************************************************************/
+void
+lockStopTest(void)
+{
+    FUNCTION_LOG_VOID(logLevelDebug);
+
+    MEM_CONTEXT_TEMP_BEGIN()
+    {
+        // Check the current stanza (if any)
+        if (cfgOptionTest(cfgOptStanza))
+        {
+            if (storageExistsP(storageLocal(), lockStopFileName(cfgOptionStr(cfgOptStanza))))
+                THROW_FMT(StopError, "stop file exists for stanza %s", strZ(cfgOptionDisplay(cfgOptStanza)));
+        }
+
+        // Check all stanzas
+        if (storageExistsP(storageLocal(), lockStopFileName(NULL)))
+            THROW(StopError, "stop file exists for all stanzas");
+    }
+    MEM_CONTEXT_TEMP_END();
+
+    FUNCTION_LOG_RETURN_VOID();
+}
---
+++ src/control/start.c
@@ -0,0 +1,39 @@
+/***********************************************************************************************************************************
+Start Command
+***********************************************************************************************************************************/
+#include "build.auto.h"
+
+#include "command/control/common.h"
+#include "common/debug.h"
+#include "config/config.h"
+#include "storage/helper.h"
+#include "storage/storage.h"
+
+/**********************************************************************************************************************************/
+void
+cmdStart(void)
+{
+    FUNCTION_LOG_VOID(logLevelDebug);
+
+    MEM_CONTEXT_TEMP_BEGIN()
+    {
+        // Remove the stop file so processes can run
+        String *stopFile = lockStopFileName(cfgOptionStrNull(cfgOptStanza));
+
+        // If the stop file exists, then remove it
+        if (storageExistsP(storageLocal(), stopFile))
+        {
+            // If the file cannot be removed, storageRemove() will throw an error if the error is not ENOENT
+            storageRemoveP(storageLocalWrite(), stopFile);
+        }
+        else
+        {
+            LOG_WARN_FMT(
+                "stop file does not exist%s",
+                (cfgOptionTest(cfgOptStanza) ? strZ(strNewFmt(" for stanza %s", strZ(cfgOptionDisplay(cfgOptStanza)))) : ""));
+        }
+    }
+    MEM_CONTEXT_TEMP_END();
+
+    FUNCTION_LOG_RETURN_VOID();
+}
